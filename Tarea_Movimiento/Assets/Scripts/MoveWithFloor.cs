f4ae4d081f16c46d2d05e5d8d8a12673831b574b
ClaudethMatute <150222554+ClaudethMatute@users.noreply.github.com>
2024-07-19 23:37:50 -0600

    Moviendonos Junto a Plataformas y Objetos
    Moviendonos Junto a Plataformas y Objetos (Falta que la camara siga al minios)

---
+++ Tarea_Movimiento/Assets/Scripts/MoveWithFloor.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWithFloor : MonoBehaviour
+{
+    CharacterController player;
+
+    Vector3 groundPosition;
+    Vector3 lastGroundPosition;
+    string groundName;
+    string lastGroundName;
+
+    Quaternion actualRot;
+    Quaternion lastRot;
+
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(player.isGrounded)
+        {
+            RaycastHit hit;
+
+            if(Physics.SphereCast(transform.position, player.height / 4.2f, -transform.up, out hit))
+            {
+                GameObject groundedIn = hit.collider.gameObject;
+                groundName = groundedIn.name;
+                groundPosition = groundedIn.transform.position;
+
+                actualRot = groundedIn.transform.rotation;
+
+                if (groundPosition != lastGroundPosition && groundName == lastGroundName)
+                {
+                    this.transform.position += groundPosition - lastGroundPosition;
+                    player.enabled = false;
+                    player.transform.position = this.transform.position;
+                    player.enabled = true;
+                }
+
+                if (actualRot != lastRot && groundName == lastGroundName)
+                {
+                    var newRot = this.transform.rotation * (actualRot.eulerAngles - lastRot.eulerAngles);
+                    this.transform.RotateAround(groundedIn.transform.position, Vector3.up, newRot.y);
+                }
+
+                lastGroundName = groundName;
+                lastGroundPosition = groundPosition;
+                lastRot = actualRot;
+
+            }
+        }
+        else if (!player.isGrounded)
+        {
+            lastGroundName = null;
+            lastGroundPosition = Vector3.zero;
+            lastRot = Quaternion.Euler(0, 0, 0);
+        }    
+    }
+
+    private void OnDrawGizmos()
+    {
+        player = this.GetComponent<CharacterController>();
+        Gizmos.DrawWireSphere(transform.position, player.height / 4.2f);
+    }
+}
